395fb895269c2a1ef45e60780c12569c0e966271
Mathias Verraes <mathias@verraes.net>
2011-05-04 13:30:52 +0200

    replaced dependencies with Goutte

--- app/autoload.php
+++ app/autoload.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once __DIR__.'/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../vendor/goutte/autoload.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'	=> __DIR__.'/../vendor/',
-    'Scanner'	=> __DIR__.'/../src/',
-	'Zend'		=> __DIR__.'/../vendor/zend/library/',
-    'Test'		=> __DIR__.'/../tests/',
+	'Scanner'	=> __DIR__.'/../src/',
+	'Test'		=> __DIR__.'/../tests/',
+	'Symfony'	=> __DIR__.'/../vendor/',
 ));
 $loader->register();
