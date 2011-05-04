300d5e074762dbad265e3a0410dd61aac90052c3
Mathias Verraes <mathias@verraes.net>
2011-05-04 10:40:15 +0200

    test site

---
+++ tests/examplesite/goodform.php
@@ -0,0 +1,33 @@
+<?php
+$message = '';
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	if($_POST['token'] != 'randomtokenxyz') {
+		header("HTTP/1.0 403 Forbidden");
+		die('invalid token');
+	}
+
+	// ... store email
+
+	$message = 'Success';
+}
+
+?>
+<html>
+<head>
+<title>Good Form</title>
+</head>
+<body>
+	<p><?php echo $message ?></p>
+	<form name="goodform" method="POST">
+		<input type="text" name="email" />
+		<input type="hidden" name="token" value="randomtokenxyz" />
+	</form>
+	<br />
+	<ul>
+		<li><a href="goodform.php">Good form</a></li>
+		<li><a href="notokenform.php">Form without token</a></li>
+		<li><a href="tokennotcheckedform.php">Token not checked</a></li>
+	</ul>
+</body>
+</html>
---
+++ tests/examplesite/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>My example site</title>
+</head>
+<body>
+	<ul>
+		<li><a href="goodform.php">Good form</a></li>
+		<li><a href="notokenform.php">Form without token</a></li>
+		<li><a href="tokennotcheckedform.php">Token not checked</a></li>
+	</ul>
+</body>
+</html>
