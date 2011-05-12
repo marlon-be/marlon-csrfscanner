4fe9f8b2bf4f1c00790b0c327ccdf0d5fcda7b38
Mathias Verraes <mathias@verraes.net>
2011-05-12 13:46:00 +0200

    FormsCollection enforces unique forms

--- src/Scanner/Collection/FormsCollection.php
+++ src/Scanner/Collection/FormsCollection.php
@@ -14,12 +14,34 @@ class FormsCollection extends ArrayCollection
 		}
 	}
 
-	public function add($value)
+	private function check(Form $value)
 	{
-		if(!$value instanceof Form)
-		{
-			throw new \InvalidArgumentException("The form must be an instance of Symfony\Component\DomCrawler\Form");
-		}
-		parent::add($value);
+		// empty, the type hinting will check if $value is a Page instance
+	}
+
+    public function add($value)
+    {
+    	// make sure forms are unique
+    	$hash = $this->getHash($value);
+        return $this->set($hash, $value);
+    }
+
+    public function set($key, $value)
+    {
+    	$this->check($value);
+		return parent::set($key, $value);
+    }
+
+	private function getHash(Form $form)
+	{
+		$fields = array();
+    	foreach($form->all() as $field) {
+    		$fields[] = array(get_class($field), $field->getName());
+    	}
+
+    	$meta = array($fields, $form->getMethod(), $form->getUri());
+    	$hash = md5(serialize($meta));
+
+    	return $hash;
 	}
 }
